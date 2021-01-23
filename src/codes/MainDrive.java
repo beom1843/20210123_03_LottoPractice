37b713f5ffc97d33520056db1c22eac3d4d6a286
beom1843cho <beom1843@gmail.com>
2021-01-23 12:15:35 +0900

    기존 입력값과 중복인지

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -6,40 +6,59 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 //		 깃허브 연동 프로젝트
-	
+
 		Scanner myScanner = new Scanner(System.in);
 
 //		6개의 숫자를 저장할 배열 만들기
 		int arr[] = new int[6];
 //		횟수를 6번으로 제한할 변수
-		
-		for( int i = 0 ; i < arr.length ; i++) {
 
-			while(true) {
+		for (int i = 0; i < arr.length; i++) {
+
+			while (true) {
+
 //			숫자입력코드
-			System.out.println((i+1)+"번째 로또 번호 입력 : ");
-			int input = myScanner.nextInt();
+				System.out.println((i + 1) + "번째 로또 번호 입력 : ");
+				int input = myScanner.nextInt();
 //			입력해도 되는 숫자인지 검사
 //			검사1 : 입력받은 숫자가 1~45의 값인가
-			boolean isRangeOk = (1<= input && input <=45) ;
-			
-			if (isRangeOk) {
+				boolean isRangeOk = (1 <= input && input <= 45);
+				
+//				추가 검사 : 중복인지 아닌지.
+//				중복인가? : 내가 입력한 숫자가 이미 배열안에 들어있다면 ? 중복(사용불가)
+				
+//				중복검사 결과
+				boolean isDuplOk = true;
+				
+//				배열 안의 숫자를 모두 꺼내보자.
+				
+				for(int arrNum : arr) {
+//					입력한 값과 arrNum이 같다면 중복검사에서 탈락
+					if(input == arrNum) {
+//						중복값 발견 ! 검사 탈락 처리
+						isDuplOk = false;
+//						중복 확정이면 for문 종료
+						break;
+					}
+				}
+				
+				if (isRangeOk) {
 //			검사2 : 
 //			배열에 저장
 
-			arr[i] = input;
+					arr[i] = input;
 //			올바른 결과가 들어갔으면 while문을 종료.
 //			다음 자리의 숫자를 받으러 이동.
-			break;
-		}
-			else {
-				System.out.println("다시 입력하세요");
+					break;
+				} else {
+					System.out.println("다시 입력하세요");
+				}
 			}
+
+			for (int num : arr) {
+				System.out.println(num);
 			}
-		for(int num : arr) {
-			System.out.println(num);
-		}
-		
+
 		}
 	}
 }
