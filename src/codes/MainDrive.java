c69ce47c0a2bbc3057a7de8c0791fc0965db262d
beom1843cho <beom1843@gmail.com>
2021-01-23 12:43:20 +0900

    숫자 오름차순 정렬 버블소트

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -65,6 +65,17 @@ public class MainDrive {
 
 
 		}
+		
+		for(int i = 0 ; i<arr.length-1 ; i++) {
+			for(int j = 0 ; j <arr.length-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					int backup = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=backup;
+				}
+			}
+		}
+		
 		for (int num : arr) {
 			System.out.println(num);
 		}
